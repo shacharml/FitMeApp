c12b20bf643ba9107a8fb8c682ae37a325021d9b
Shachar <Shacharml611@gmail.com>
2023-07-04 14:00:07 +0300

    Initial Commit

---
+++ FitMeApp/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  FitMeApp
+//
+//  Created by Student30 on 04/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Image("fitness-workout")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(height: 300)
+            
+            
+            //Vertical Stack
+            VStack {
+                
+                Spacer()
+                
+                
+               Text("Full Body Workout")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                Text("For Beginners")
+                    .fontWeight(.regular)
+                    .foregroundColor(.white)
+            }
+            .padding()
+            .frame(width: 380)
+            .background(Color(hue: 1.0, saturation: 0.048, brightness: 0.28, opacity: 0.384))
+        }
+        .frame(width: 380 , height: 300)
+        .cornerRadius(20)
+        .clipped()
+        .shadow(radius: 8)
+        .padding(.top,20)
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FitMeApp/FitMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitMeAppApp.swift
+//  FitMeApp
+//
+//  Created by Student30 on 04/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct FitMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
